49e7c3687d031a4c248dcb3f2178b818ac38c4a4
mrsmarah <marahabdallah91@gmail.com>
2020-02-05 13:49:58 +0200

    bugs

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-var experience = prompt("How much experience years do you have ?");
-console.log(experience);
-var result; 
- 
-if (experience >= 5 ) {     
-    result = 'Your apportunity to get this job is High';   
-} 
-else if (experience < 5 && experience >= 2 ) {     
-    result = 'please submit your CV to schedule the interview';   
-} else if (experience < 2 && experience >= 0) {     
-    result = 'Sorry! You are not qualified enough for this specific position.';   
-} else {     
-    result = 'Something went wrong!';   
-} 
+    var experience = prompt("How much experience years do you have ?");
+    console.log(experience);
+    var overtime = prompt("Can you work for an overtime ?");
+    console.log(overtime);
+    var pressure = prompt("Can you work under pressure ?");
+    console.log(pressure);
+    var result; 
 
-document.write('<h3>'+result+'</h3>');
+
+function job () 
+{
+     
+    
+    if (experience >= 5 && overtime == "yes" && pressure == "yes" ) {     
+        result = 'Your apportunity to get this job is High';   
+    } 
+    else if (experience < 5 && experience >= 2 && overtime == "yes" && pressure == "yes"  ) {     
+        result = 'please submit your CV to schedule the interview';   
+    } else if (experience < 2 && experience >= 0 || overtime == "no" || pressure == "no") {     
+        result = 'Sorry! You are not qualified enough for this specific position.';   
+    } else {     
+    
+        result = 'Something went wrong!';   
+    } 
+    
+    
+}
+
+document.write('<h3>'result()'</h3>');
